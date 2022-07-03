b3d7ea17ac2e259f6752a04da66a8d2db8cfaf8d
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2022-07-04 00:44:16 +0800

    Implement insertionSort

---
+++ ch2_insertion_sort.nim
@@ -0,0 +1,9 @@
+proc insertionSort*[T](keys: var openArray[T]) =
+  for i in 1..keys.len-1:
+    let key = keys[i]
+    # Insert keys[i] into sorted subarray keys[0..i-1]
+    var j = i - 1
+    while j >= 0 and keys[j] > key:
+      keys[j + 1] = keys[j]
+      j = j - 1
+    keys[j + 1] = key
