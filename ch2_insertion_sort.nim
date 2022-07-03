1cde51afe5cc6ee7e3f125daaa1fd9c33181e106
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2022-07-04 01:08:36 +0800

    Refactor

--- ch2_insertion_sort.nim
+++ ch2_insertion_sort.nim
@@ -1,6 +1,5 @@
 proc insertionSort*[T](keys: var openArray[T]) =
-  for i in 1..keys.len-1:
-    let key = keys[i]
+  for i, key in keys:
     # Insert keys[i] into sorted subarray keys[0..i-1]
     var j = i - 1
     while j >= 0 and keys[j] > key:
